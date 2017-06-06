2d0ab328a19a0d3e5a3c2d1816540fc6382ce4e7
Evan Kimia <evan@zerofinancial.com>
2017-06-06 13:42:06 -0700

    Add support for Carthage.

--- Example/PIDatePicker/PIViewController.swift
+++ Example/PIDatePicker/PIViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import PIDatePicker
 
+
 class PIViewController : UIViewController {
     
     @IBOutlet weak var datePicker: PIDatePicker!
---
+++ PIDatePicker/PIDatePicker/PIDatePicker.h
@@ -0,0 +1,19 @@
+//
+//  PIDatePicker.h
+//  PIDatePicker
+//
+//  Created by Evan Kimia on 6/6/17.
+//  Copyright © 2017 Zero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PIDatePicker.
+FOUNDATION_EXPORT double PIDatePickerVersionNumber;
+
+//! Project version string for PIDatePicker.
+FOUNDATION_EXPORT const unsigned char PIDatePickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PIDatePicker/PublicHeader.h>
+
+
